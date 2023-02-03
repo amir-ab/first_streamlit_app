1483e3bd44f382fbb2833c61b5a4393e5219cdc8
Amir Abbes <abbes.amir@yahoo.com>
2023-02-03 06:56:47 +0100

    Update streamlit_app.py
    fixed typo "Avocado"

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ streamlit.text('🥑🍞 Avocado toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 #Let's put a pick list here so they can pick the fruit they want to include
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avodaco', 'Strawberries'])
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
 
 
 #display the table on the page
