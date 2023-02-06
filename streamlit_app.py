0f559f3f4836b68ffe42bf98a63e13782a2d2437
Amir Abbes <abbes.amir@yahoo.com>
2023-02-06 11:42:45 +0100

    Update streamlit_app.py
    bug typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,9 @@ import streamlit
 import pandas as p
 import requests
 
-fruityvice_reponse = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityuvice_reponse)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+
 
 my_fruit_list = p.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
