91ceced6910bddb3fee5980ff26137b3b2685d46
Amir Abbes <abbes.amir@yahoo.com>
2023-02-06 11:45:26 +0100

    Update streamlit_app.py
    bug typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,6 @@ import streamlit
 import pandas as p
 import requests
 
-#New Section to display fruity api reponse
-streamlit.header('fruityvice FRUIT Advice')
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
 
 
 my_fruit_list = p.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
@@ -30,4 +25,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 #display the table on the page
 streamlit.dataframe(fruits_to_show)
+
+
+
+#New Section to display fruity api reponse
+streamlit.header("Fruityvice Fruit Advice!")
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
 
