749899919a8ed49991d9bb415469eaa6b1eaa96e
Amir Abbes <abbes.amir@yahoo.com>
2023-02-07 02:36:27 +0100

    Update streamlit_app.py
    hope

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,16 +32,20 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display fruity api reponse
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
-streamlit.write('The user entred', fruit_choice)
-
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-
-# take the json version of the response and normalize it
-fruityvice_normalized = p.json_normalize(fruityvice_response.json())
-#output it the screen as a table
-streamlit.dataframe(fruityvice_normalized)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information")
+  else:
+    streamlit.write('The user entred', fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+    # take the json version of the response and normalize it
+    fruityvice_normalized = p.json_normalize(fruityvice_response.json())
+    #output it the screen as a table
+    streamlit.dataframe(fruityvice_normalized)
+    
+except URLError as e:
+  streamlit.error()
 
 streamlit.stop()
 
