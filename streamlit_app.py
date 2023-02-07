70547c8f31cce75490c40b7e2bd480f17b8d1565
Amir Abbes <abbes.amir@yahoo.com>
2023-02-07 13:02:22 +0100

    Update streamlit_app.py
    bug typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,10 @@ from urllib.error import URLError
 
 
 
+
 my_fruit_list = p.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
+
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -20,19 +22,21 @@ streamlit.text('🥑🍞 Avocado toast')
 
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
 #Let's put a pick list here so they can pick the fruit they want to include
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
+
 #display the table on the page
 streamlit.dataframe(fruits_to_show)
+
 
 #Create the repeatable code block
 def get_fruityvice_data(this_fruit_choice):
   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ this_fruit_choice)
   fruityvice_normalized = p.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
+
 
 #New Section to display fruity api reponse
 streamlit.header("Fruityvice Fruit Advice!")
@@ -56,7 +60,7 @@ def get_fruit_load_list():
     return my_cur.fetchall()
 
 #Add a button to load the fruit
-  if streamlit.button('Get Fruit Load List'):
+if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
